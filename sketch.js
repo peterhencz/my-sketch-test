290c8981e3b75862d7579f25109114e76a2dd60d
Peter Hencz <peter.hencz@gmail.com>
2019-05-24 21:45:51 +0200

    save

--- sketch.js
+++ sketch.js
@@ -1,13 +1,35 @@
-const canvasSketch = require('canvas-sketch');
+const canvasSketch = require("canvas-sketch");
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  animate: true,
+  duration: 3,
+  dimensions: [600, 600],
+  fps: 24,
 };
 
-const sketch = () => {
-  return ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
+function handler(e) {
+  e = e || window.event;
+
+  var pageX = e.pageX;
+  var pageY = e.pageY;
+
+  // IE 8
+  if (pageX === undefined) {
+    pageX =
+      e.clientX +
+      document.body.scrollLeft +
+      document.documentElement.scrollLeft;
+    pageY =
+      e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+  }
+  console.log(pageX, pageY);
+}
+if (document.attachEvent) document.attachEvent("mousemove", handler);
+else document.addEventListener("mousemove", handler);
+sketch = () => {
+  return ({ context, width, height, playhead }) => {
+    context.fillStyle = "pink";
+    context.fillRect(0, 0, 300, 300);
   };
 };
 
